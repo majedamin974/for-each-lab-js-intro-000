e02c89be2a4dd9f1cc4d49d385da520f6cf24a05
Majed Amin <majedamin974@gmail.com>
2021-02-13 21:00:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function iterativeLog() {
-
+function iterativeLog(array) {
+  array.forEach(index => () {
+    console.log(`${index}: ${element}`);
+  });
 }
+
+function iterate(callback) {
+  
+}
+
+function doToArray(array, callback) {
+  
+}
+
+iterativeLog([1,2,3,4,5,6]);
