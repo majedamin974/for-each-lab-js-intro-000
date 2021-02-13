6f6e57b0f79004d04075b76a250b14ef47292287
Majed Amin <majedamin974@gmail.com>
2021-02-13 22:05:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+let array = [1,2,3,4,5,6];
+
+function iterativeLog(arr) {
+  arr.forEach((element, index)=> {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  let arr = [10, 20, 30];
+  arr.forEach(callback);
+  return arr;
+}
+
+function addTen(el, index, arr) {
+  arr[index] = el + 10;
+}
+
+function doToArray(arr, callback) {
+  arr.forEach(callback);
+}
+
+doToArray(array, addTen);
+console.log(array);
